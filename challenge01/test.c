2a505a4f327aa3f6d5417323d86f6ce6b5c7d672
PPS10198054 <juamarmar12@alu.edu.gva.es>
2025-11-05 16:53:00 +0100

    Arreglado challenge01
    Updated test cases for suma function and added output.

--- challenge01/test.c
+++ challenge01/test.c
@@ -4,14 +4,15 @@
 int suma(int n1, int n2);
 
 void test_suma() {
-	assert(suma(6,3) == 9);
+        assert(suma(6,3) == 9);
 
-	assert(suma(8,3) == 11);
+        assert(suma(-8,3) == -5);
 
-	assert(suma(3,1) == 4);
+        assert(suma(-3,-1) == -4);
 };
 
 int main() {
-	test_suma();
-	return 0;
+        test_suma();
+        printf("OK\n");
+        return 0;
 }
