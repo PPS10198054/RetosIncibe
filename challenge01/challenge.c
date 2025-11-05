e218a8d6fc4fdc392c06bfa97c3b6e61392f57ac
PPS10198054 <juamarmar12@alu.edu.gva.es>
2025-11-05 15:46:15 +0100

    Añadido el programa en C con el test unitario

---
+++ challenge01/challenge.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int suma(int n1, int n2) {
+	return n1 + n2; 
+}
+
---
+++ challenge01/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <assert.h>
+
+int suma(int n1, int n2);
+
+void test_suma() {
+	assert(suma(6,3) == 9);
+
+	assert(suma(8,3) == 11);
+
+	assert(suma(3,1) == 4);
+};
+
+int main() {
+	test_suma();
+	return 0;
+}
